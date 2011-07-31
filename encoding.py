dc62f2e02681805bc90511542d30d84c59ff19f2
Steve R <steven.richardspc@gmail.com>
2011-07-31 14:11:10 -0700

    Added prints for finding fatal error in python, seems like you can't play too many tones without it crashing?

--- encoding.py
+++ encoding.py
@@ -5,10 +5,12 @@ device = audiere.open_device()
 openfile = raw_input("Enter path to file: ")
 f = open(openfile)
 for line in f:
- each_charline = [line]
- for each_charline in line:
-   tone = device.create_tone(ord(each_charline) + 500)
-   tone.play()
+ each_charline = [line] 
+for each_charline in line:
+    #print(ord(each_charline))  \-Debugging prints for  
+    #print(each_charline)       /-Fatal error :(
+    tone = device.create_tone(ord(each_charline))
+    tone.play()
 f.close()
 ####print(bin(reduce(lambda x, y: 256*x+y, (ord(c) for c in n), 0))) <-- Binary Only
                                   
