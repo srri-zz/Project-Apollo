8cdcf70478a74a1fa89d9728235d55cedd919156
Steve R <steven.richardspc@gmail.com>
2011-08-04 13:39:59 -0700

    semi-working decoder

--- decoding.py
+++ decoding.py
@@ -1,12 +1,16 @@
+#Decoder code modified from Sample code for PyAudio
+
 import pyaudio
 import wave
 import sys
+import numpy as np
 
-chunk = 1024
-FORMAT = pyaudio.paInt32
+
+chunk = 2048
+FORMAT = pyaudio.paInt16
 CHANNELS = 1
 RATE = 44100
-RECORD_SECONDS = 5
+RECORD_SECONDS = raw_input("How Many Seconds would you like to record?: ")
 WAVE_OUTPUT_FILENAME = "output.wav"
 
 p = pyaudio.PyAudio()
@@ -19,9 +23,8 @@ stream = p.open(format = FORMAT,
 
 print "* recording"
 all = []
-for i in range(0, RATE / chunk * RECORD_SECONDS):
-    data = stream.read(chunk - 1000)
-    print(ord(data))
+for i in range(0, RATE / chunk * int(RECORD_SECONDS)):
+    data = stream.read(chunk)
     all.append(data)
 print "* done recording"
 
@@ -36,3 +39,47 @@ wf.setsampwidth(p.get_sample_size(FORMAT))
 wf.setframerate(RATE)
 wf.writeframes(data)
 wf.close()
+
+# open up a wave
+wf = wave.open('output.wav', 'rb')
+swidth = wf.getsampwidth()
+RATE = wf.getframerate()
+# use a Blackman window
+window = np.blackman(chunk)
+# open stream
+p = pyaudio.PyAudio()
+stream = p.open(format =
+                p.get_format_from_width(wf.getsampwidth()),
+                channels = wf.getnchannels(),
+                rate = RATE,
+                output = True)
+
+# read some data
+data = wf.readframes(chunk)
+# play stream and find the frequency of each chunk
+while len(data) == chunk*swidth:
+    # write data out to the audio stream
+    stream.write(data)
+    # unpack the data and times by the hamming window
+    indata = np.array(wave.struct.unpack("%dh"%(len(data)/swidth),\
+                                         data))*window
+    # Take the fft and square each value
+    fftData=abs(np.fft.rfft(indata))**2
+    # find the maximum
+    which = fftData[1:].argmax() + 1
+    # use quadratic interpolation around the max
+    if which != len(fftData)-1:
+        y0,y1,y2 = np.log(fftData[which-1:which+2:])
+        x1 = (y2 - y0) * .5 / (2 * y1 - y2 - y0)
+        # find the frequency and output it
+        thefreq = (which+x1)*RATE/chunk
+        print (chr(int(thefreq - 1000)))
+    else:
+        thefreq = which*RATE/chunk
+        print (chr(int(thefreq - 1000)))
+    # read some more data
+    data = wf.readframes(chunk)
+if data:
+    stream.write(data)
+stream.close()
+p.terminate()
