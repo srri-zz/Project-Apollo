f4d776625df9de8be7c35c1f250bbb26e7331fdc
Steve R <steven.richardspc@gmail.com>
2011-08-13 20:13:35 -0700

    fixed some things

--- encoding.py
+++ encoding.py
@@ -1,12 +1,13 @@
 #By Steven Richards
 #ASCII encoder to frequency tone
 import audiere
-import bz2
+#import bz2
 from itertools import izip, cycle
 device = audiere.open_device()
 
 def xor_crypt_string(data, key):
     return ''.join(chr(ord(x) ^ ord(y)) for (x,y) in izip(data, cycle(key)))
+
 openfile = raw_input("Enter path to file: ")
 f = open(openfile)
 userkey = raw_input("Enter a random key: ")
@@ -14,7 +15,7 @@ for line in f:
  #each_charline = [line]
  #enc_line = bz2.compress(line) #compression
  #each_charline = [enc_line] 
- enc_line = xor_crypt_string(line, userkey) #XOR'd OTP Encryption
+ enc_line = xor_crypt_string(line, userkey) #XOR'd Encryption
  each_charline = [enc_line]
  for each_charline in enc_line:
   print(ord(each_charline))  #\-Debugging prints for  
