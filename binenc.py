eb7641a4ed97fbf6511af76734856170eb4e2911
Steve R <steven.richardspc@gmail.com>
2011-09-17 11:11:43 -0700

    added a ton of features, integrated new file type .laze

--- binenc.py
+++ binenc.py
@@ -9,39 +9,102 @@ import os
 from Crypto.Cipher import AES
 from time import sleep
 from math import sqrt
-#key = raw_input("Enter a key with a length of 16, or 32 Characters: ")
-#mode = AES.MODE_ECB #ECB AES
-#encryptor = AES.new(key, mode)
-device = audiere.open_device()#Open and assign the audio device
-
-openfile = raw_input("Enter path to file: ")#File to encrypt
-f = open(openfile)
-file_data = f.read()#Read whole file into memory
-
-#if len(key) == 16:
-#  file_data += "\n" * (16-len(file_data) % 16)#Make length of file data 16 
-#else:
-#  file_data += "\n" * (32-len(file_data) % 32)#Make length of file data 32
-
-#enc_file = base64.b64encode(file_data)#Encrypt
-#print enc_file
-enc_file = bin(int(binascii.hexlify(file_data), 16))
-print enc_file
-enc_file = str(enc_file[2:])
-print enc_file
 
 def new_frequency(char):
-    sleep(0.058252427144)
-    if char == '1':
-        tone = device.create_tone(450.00)
-        tone.play()
-        print '450 = 1'
-    if char == '0':
-        tone = device.create_tone(350.00)
-        tone.play()
-        print '350 = 0'
-    
+            if char == '1':
+                tone = device.create_tone(450.00)
+                tone.play()
+                sleep(1.00/17.00)
+                tone.stop()
+            if char == '0':
+                tone = device.create_tone(350.00)
+                tone.play()
+                sleep(1.00/17.00)
+                tone.stop()
 
-for char in str(enc_file):
-  new_frequency(char)
-f.close()
+def message(exist, messageold):
+        print "\n\nMessage Entry \n\n"
+##        key = raw_input("Enter a key with a length of 16, or 32 Characters: ")
+##	mode = AES.MODE_ECB #ECB AES
+##	encryptor = AES.new(key, mode)
+        device = audiere.open_device()#Open and assign the audio device
+        if exist == 1:
+                binmessage = bin(int(binascii.hexlify(messageold), 16))
+                binmessage = str(binmessage[2:])
+                raw_input("Press enter to send " + messageold)
+                print binmessage
+                for char in binmessage:
+                  new_frequency(char)
+                  print char
+        else:
+                message = raw_input("Enter your message: \n")
+        ##        if len(key) == 16:
+        ## 		file_data += "\n" * (16-len(file_data) % 16)#Make length of file data 16 
+        ##	else:
+        ##  		file_data += "\n" * (32-len(file_data) % 32)#Make length of file data 32
+                header = 'DATA:MESSAGE'
+                messagefix = header + message
+                binmessage = bin(int(binascii.hexlify(messagefix), 16))
+                binmessage = str(binmessage[2:])
+                raw_input("Press enter to send '" + message + "'")
+                print binmessage
+                for char in binmessage:
+                  new_frequency(char)
+                  print char
+        
+def filetrans(exist, filedata):
+        print "\n\nFile Sending\n\n"
+##        key = raw_input("Enter a key with a length of 16, or 32 Characters: ")
+##        mode = AES.MODE_ECB #ECB AES
+##        encryptor = AES.new(key, mode)
+        device = audiere.open_device()#Open and assign the audio device
+        if exist == 1:
+                binfile = bin(int(binascii.hexlify(filedata), 16))
+                binfile = str(binfile[2:])
+                raw_input("Press enter to send the file")
+                for char in binfile:
+                  new_frequency(char)
+                  print char
+        else:
+                openfile = raw_input("Enter path to file: ")#File to encrypt
+                f = open(openfile)
+                file_data = f.read()#Read whole file into memory
+
+                ##        if len(key) == 16:
+                ##                file_data += "\n" * (16-len(file_data) % 16)#Make length of file data 16 
+                ##        else:
+                ##                file_data += "\n" * (32-len(file_data) % 32)#Make length of file data 32
+                ##
+                #enc_file = base64.b64encode(file_data)#Encrypt
+                #print enc_file
+
+                header = "DATA:FILE"
+                enc_file = bin(int(binascii.hexlify(file_data), 16))
+                print enc_file
+                enc_file = str(enc_file[2:])
+                print enc_file
+
+                for char in str(enc_file):
+                  new_frequency(char)
+                f.close()
+
+def openlaze():
+        print "\n\nOpening a Laze File\n\n"
+        fileloc = raw_input("Enter path to file: ")#File to read
+        laze = open(fileloc)
+        laze_data = str(laze.read())
+        if str(laze_data.find('DATA:MESSAGE')) == '0':
+                message(1, laze_data)
+        if str(laze_data.find('DATA:FILE')) == '0':
+                filetrans(1, laze_data)
+
+                
+userin = raw_input("Enter\n 1 to send a message\n 2 to send a file \n 3 to enter path to a .laze file: \n ")
+
+if userin == '1':
+        message(0, 0)
+if userin == '2':
+        filetrans(0, 0)
+if userin == '3':
+        openlaze()
+
