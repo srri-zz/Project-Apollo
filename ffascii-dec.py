f9508b37a54981324c6df783fe2b7ecf616739c4
Steve R <steven.richardspc@gmail.com>
2012-01-04 21:43:45 -0800

    whoops, indentation

--- ffascii-dec.py
+++ ffascii-dec.py
@@ -1,5 +1,5 @@
-#Single Toned Fascii Decoding
-#Copyright 2011-2012 Steven Richards <sbrichards@mit.edu>
+#Author Steven Richards <sbrichards@mit.edu>
+#Fascii Decoding over Laser/Audio
 
 import pyaudio
 import wave
@@ -87,32 +87,29 @@ while len(data) == chunk*swidth:
     # find the maximum
     which = fftData[1:].argmax() + 1
     # use quadratic interpolation around the max
-    start = 1
     if which != len(fftData)-1:
         y0,y1,y2 = np.log(fftData[which-1:which+2:])
         x1 = (y2 - y0) * .5 / (2 * y1 - y2 - y0)
         # find the frequency and output it
         thefreq = (which+x1)*RATE/chunk
-        
         if int(round(thefreq, -2)) > 2000:
-        	found = 'true'
-                if start == 1:
-                        start = 0
-                        currentmode = 2
-                        juststarted = 1
-                else:
-                        currentmode += 1
-		
+          found = 'true'
+          if start == 1:
+              start = 0
+              currentmode = 2
+              juststarted = 1
+          else:
+              currentmode += 1
         if found == 'true' and (int(round(thefreq, -2)) != (lasttone) or juststarted == 1):
                 if currentmode % 2 == 0:
-			tempvar = (int(round(thefreq, -2)) - 2100) / 100
-        	        outputstring += chr(tempvar)
-			lasttone = int(round(thefreq, -2))
-		else:
-			tempvar = (int(round(thefreq, -2)) - 1900) / 100
-			outputstring += chr(tempvar)
-			lasttone = int(round(thefreq, -2))
-		juststarted = 0
+                    tempvar = (int(round(thefreq, -2)) - 2100) / 100
+                    outputstring += chr(tempvar)
+                    lasttone = int(round(thefreq, -2))
+                else:
+                    tempvar = (int(round(thefreq, -2)) - 1900) / 100
+                    outputstring += chr(tempvar)
+                    lasttone = int(round(thefreq, -2))
+                juststarted = 0
     # read some more data
     data = wf.readframes(chunk)
 if data:
@@ -139,8 +136,4 @@ print outputstring
 ##	raw_input("write successful, press enter to close")
 ##	exit()
 
-
-
-
-
-
+		
